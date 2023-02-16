0046a7bee1021265ac62801d7d7f3517573d2752
nicodemuskoech <125378609+nicodemuskoech@users.noreply.github.com>
2023-02-16 00:58:51 -0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //A program to solve quadratic equations
+
+    float a,b,c,root1,root2;
+
+    printf("Enter the value of a,\n");
+    scanf("%f",$a);//It stores value of a
+
+    printf("Enter the value of b,\n");
+    scanf("%f"$b);//It stores the value of b
+
+    printf("Enter the value of c,\n");
+    scanf("%f"$c);//It stores the value of c
+
+    root1=(-b+sqrt(b*b-4*a*c))/(2*a);//For the first root of x
+    root2=(-b=sqrt(b*b-4*a*c))/(2*a);//For the second root of x
+
+    printf("%f value of root 1,\n",root 1);
+    printf("%f value of root 2,\n,root 2);
+
+
+
+    rturn 0;
+
+}
